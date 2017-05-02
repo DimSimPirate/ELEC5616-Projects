b6067075bc678c389232efe753e9f2190de2145b
Jingkai Meng <309397630@qq.com>
2017-05-02 17:14:01 +1000

    impl sign file function

--- Project1/lib/sign.py
+++ Project1/lib/sign.py
@@ -6,7 +6,7 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256
 from Crypto import Random
 from Crypto.PublicKey import RSA
-
+import os
 
 def generate_key():
     # Using RSA to generate 2048-bit keys
@@ -24,7 +24,10 @@ def generate_key():
 
 def sign_file(file):
     # sign the file with signature
-    h = SHA256.new(file.encode('utf-8'))
+    # print(file.read().encode('utf-8'))
+    f = open(os.path.join("../pastebot.net/master_folder/", file), "rb").read()
+    h = SHA256.new(f)
+    print(h.hexdigest())
     key = RSA.importKey(open('../pastebot.net/master_folder/signature_Private_key.pem').read())
     sign = PKCS1_v1_5.new(key)
     signature = sign.sign(h)
@@ -32,16 +35,22 @@ def sign_file(file):
     return signature
 
 # Demonstrate the sign and verify process
-generate_key()
-file = 'signed msg'
-h = SHA256.new(file.encode('utf-8'))
-abc = sign_file(file)
-publicKey = RSA.importKey(open('../pastebot.net/master_folder/signature_Public_key.pem').read())
-print(abc)
-verifier = PKCS1_v1_5.new(publicKey)
-if verifier.verify(h,abc):
-    print("verified")
-else:
-    print("not verified")
-
+# master side generation
+# file = 'hello.fbi'
+#
+# f = open(os.path.join("../pastebot.net/master_folder/", file), "rb").read()
+# h1 = SHA256.new(f)
+# print(h1.hexdigest())
+# signature = sign_file(file)
+#
+#
+#
+# # bot side verification
+# publicKey = RSA.importKey(open('../pastebot.net/master_folder/signature_Public_key.pem').read())
+# verifier = PKCS1_v1_5.new(publicKey)
+# if verifier.verify(h1,signature):
+#     print("verified")
+# else:
+#     print("not verified")
+# #
 
