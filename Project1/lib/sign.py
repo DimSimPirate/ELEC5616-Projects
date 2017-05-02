e4b2b6d15b57e6a865f8c3a3e3b8ba3659d69d48
Jingkai Meng <309397630@qq.com>
2017-05-02 16:25:07 +1000

    added sign_file(file) function to sign a file

--- Project1/lib/sign.py
+++ Project1/lib/sign.py
@@ -12,12 +12,36 @@ def generate_key():
     # Using RSA to generate 2048-bit keys
     key = RSA.generate(2048)
 
-    # Write the key to master_folder/signature_key.pem
+    # Write the private key
     f = open("../pastebot.net/master_folder/signature_Private_key.pem",'w')
     f.write(key.exportKey('PEM').decode('utf-8'))
     f.close()
 
-
+    # Write the public key
+    f = open("../pastebot.net/master_folder/signature_Public_key.pem",'w')
+    f.write(key.publickey().exportKey('PEM').decode('utf-8'))
+    f.close()
 
 def sign_file(file):
-    pass
+    # sign the file with signature
+    h = SHA256.new(file.encode('utf-8'))
+    key = RSA.importKey(open('../pastebot.net/master_folder/signature_Private_key.pem').read())
+    sign = PKCS1_v1_5.new(key)
+    signature = sign.sign(h)
+
+    return signature
+
+# Demonstrate the sign and verify process
+generate_key()
+file = 'signed msg'
+h = SHA256.new(file.encode('utf-8'))
+abc = sign_file(file)
+publicKey = RSA.importKey(open('../pastebot.net/master_folder/signature_Public_key.pem').read())
+print(abc)
+verifier = PKCS1_v1_5.new(publicKey)
+if verifier.verify(h,abc):
+    print("verified")
+else:
+    print("not verified")
+
+
