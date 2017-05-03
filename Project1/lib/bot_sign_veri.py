2fc6a7f1ee031bdf5863fe000561550e16385513
softwaredonald <xfdonald@gmail.com>
2017-05-03 19:03:49 +1000

    bot

---
+++ Project1/lib/bot_sign_veri.py
@@ -0,0 +1,36 @@
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import os
+
+def gen_key():
+    #for testing
+    rng = Random.new().read
+    key = RSA.generate(2048,rng)
+    key_f=open("../pastebot.net/bots_folder/bot1/sign_public_key.spki","w")
+    key_f.write(key.publickey().exportkey("spki").encode("utf-8"))
+    key_f.close()
+
+
+def bot_signfile():
+    #botnet sign files
+    f = open(os.path.join("../pastebot/bots_folder/bot1/helloworld"),"rd").read()
+    key = RSA.importKey(open('../pastebot/bots_folder/bot1/sign_public_key.spki').read())
+    hash = SHA256.new(f).digest()
+    signature = PKCS1_v1_5.new(key).sign(hash)
+    return signature
+
+
+
+def bot_verification():
+    #botnet veri master
+    publickey=RSA.importKey(open('../pastebot.net/bots_folder/bot1/sign_public_key.spki').read())
+    veri =PKCS1_v1_5.new(publickey)
+    if veri:
+        print("the file comes from master")
+    else:
+        print("it does not come from master")
+
+
+
